15625fb89cd4b9b703bb914840dd925cbdb04bdd
DanniDevv <dante.rodriguez.c@tecsup.edu.pe>
2023-12-06 12:14:35 -0500

    Login Smart-Parking v2

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 // index.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const session = require('express-session');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+// Middleware para sesiones
+app.use(session({
+    secret: 'tu_secreto_secreto',
+    resave: false,
+    saveUninitialized: true
+}));
 
 // Middleware para servir archivos estáticos desde la carpeta "assets"
 app.use('/assets', express.static('assets'));
@@ -31,6 +38,9 @@ app.post('/login', async (req, res) => {
         const { success, empresa } = response.data;
 
         if (success === 'Login correcto' && empresa) {
+            // Guardar el ID de la empresa en la sesión
+            req.session.empresaId = empresa;
+
             // Redirigir a la ruta /home con el ID de la empresa
             res.redirect(`/home/${empresa}`);
         } else {
@@ -46,8 +56,11 @@ app.post('/login', async (req, res) => {
 // Ruta para mostrar detalles de la empresa en /home
 app.get('/home/:id', async (req, res) => {
     try {
-        // Hacer solicitud a tu API de empresa con la ID proporcionada
-        const empresaResponse = await axios.get(`http://localhost:9000/api/empresa/${req.params.id}`);
+        // Utilizar el ID de la empresa almacenado en la sesión
+        const empresaId = req.session.empresaId;
+
+        // Hacer solicitud a tu API de empresa con la ID almacenada en la sesión
+        const empresaResponse = await axios.get(`http://localhost:9000/api/empresa/${empresaId}`);
 
         // Modificar la URL de la imagen para incluir el host
         const empresaDetails = {
@@ -63,9 +76,33 @@ app.get('/home/:id', async (req, res) => {
     }
 });
 
+
 // Ruta para parking
 app.get('/parking', (req, res) => {
     res.render('parking');
+});
+
+// Ruta para detalles
+app.get('/details', async (req, res) => {
+    try {
+        // Utilizar el ID de la empresa almacenado en la sesión
+        const empresaId = req.session.empresaId;
+
+        // Hacer solicitud a tu API de empresa con la ID almacenada en la sesión
+        const empresaResponse = await axios.get(`http://localhost:9000/api/empresa/${empresaId}`);
+
+        // Modificar la URL de la imagen para incluir el host
+        const empresaDetails = {
+            ...empresaResponse.data,
+            imagen: 'http://localhost:9000/' + empresaResponse.data.imagen,
+        };
+
+        // Renderizar vista de detalles con detalles de la empresa
+        res.render('details', { empresaDetails });
+    } catch (error) {
+        console.error(error);
+        res.render('login-error');
+    }
 });
 
 app.listen(port, () => {
